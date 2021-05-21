c34fd883d358c65fc3cbfd1fefc8c6305b8913fb
jod35 <jodestrevin@gmail.com>
2021-05-21 14:24:00 -0400

    databases

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Note(models.Model):
+    title=models.CharField(max_length=45,null=False)
+    description=models.TextField(null=False)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
